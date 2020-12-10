c532a81f068dfdbbe0c3a3d280bdd0023f381dd3
BrunoVictorFerreira <victorbruno221@gmail.com>
2020-12-10 17:11:13 -0300

    Handling error

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const rotaUsuarios = require('./routes/usuarios')
 const rotaProdutos = require('./routes/produtos')
 const rotaPedidos = require('./routes/pedidos')
+const morgan = require('morgan')
+
+app.use(morgan('dev'))
 
 app.use('/usuarios', rotaUsuarios)
 
@@ -10,9 +13,18 @@ app.use('/produtos', rotaProdutos)
 
 app.use('/pedidos', rotaPedidos)
 
-app.use('/teste', (req, res, next) => {
-    res.status(200).send({
-        mesangem: "Ok, deu Certo"
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado')
+    res.status(404)
+    next(erro)
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
     })
 })
 
